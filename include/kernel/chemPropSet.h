9ea7d9df250c20f8bce0cc749484e5b2d82a63b2
flong@mrc-lmb.cam.ac.uk <>
2014-07-03 17:25:37 +0100

    Add include/kernel/chemPropSet.h

---
+++ include/kernel/chemPropSet.h
@@ -0,0 +1,168 @@
+/* 
+ * File:   chemPropSet.h
+ * Author: flong
+ *
+ * Created on July 3, 2014, 1:27 PM
+ */
+
+#ifndef CHEMPROPSET_H
+#define	CHEMPROPSET_H
+
+#ifndef KERNEL_H
+#include "kernel.h"
+#endif
+
+#ifndef ATOM_H
+#include "atom.h"
+#endif
+
+#ifndef BOND_H
+#include "bond.h"
+#endif
+
+#ifndef ANGLE_H
+#include "angle.h"
+#endif
+
+#ifndef TORSION_H
+#include "torsion.h"
+#endif
+
+#ifndef RESIDUE_H
+#include "residue.h"
+#endif
+
+#ifndef RING_H
+#include "ring.h"
+#endif
+
+#ifndef PLANE_H
+#include "plane.h"
+#endif
+
+#ifndef CHAIN_H
+#include "chain.h"
+#endif
+
+#ifndef MOLECULE_H
+#include "molecule.h"
+#endif
+
+#ifndef LIBG_MODEL_H
+#include "libgmodel.h"
+#endif
+
+#ifndef SSBOND_H
+#include "ssbond.h"
+#endif
+
+#ifndef LIBG_LINK_H
+#include "libglink.h"
+#endif
+
+#ifndef CRYSTINFO_H
+#include "crystInfo.h"
+#endif
+
+#ifndef SECONDARYSTRUCTURES_H
+#include "secondaryStructures.h"
+#endif
+
+#ifndef CODCLASSIFY_H
+#include "codClassify.h"
+#endif
+
+#ifndef UTILITY_H
+#include "utility.h"
+#endif
+
+#ifndef TRANSCOORD_H
+#include "TransCoord.h"
+#endif
+
+namespace LIBMOL
+{
+    
+        class Atom;
+    class AtomDict;
+    
+    class Bond;
+    class BondDict;
+    
+    class Ring;
+    class RingDict;
+    
+    class Angle;
+    class AngleDict;
+    
+    class Torsion;
+    class TorsionDict;
+    
+    class Chiral;
+    class ChiralDict;
+    
+    class Plane;
+    class PlaneDict;
+    
+    class Molecule;
+    
+    class CodClassify;
+    
+    class CCP4AtomType;
+    
+    extern int getNumOxyConnect(std::vector<AtomDict>  &  tAtoms,
+                                std::vector<AtomDict>::iterator iA);
+    
+    extern void setAtomsBondingAndChiralCenter(std::vector<AtomDict> & tAtoms);
+    
+    extern REAL checkProtonateO(std::vector<AtomDict>::iterator tIA, 
+                                REAL tTolBondOrder);
+    extern REAL checkProtonateO(std::vector<AtomDict>::iterator tIA, 
+                                Molecule   & tMol);
+    extern REAL checkProtonateO(std::vector<AtomDict>::iterator tIA, 
+                                Molecule   & tMol,
+                                REAL tPka,           REAL tPh);
+    extern REAL checkProtonateN(std::vector<AtomDict>::iterator tIA, 
+                                REAL tTolBondOrder,
+                                std::vector<AtomDict> &tAllAtoms);
+    extern REAL checkProtonateN(std::vector<AtomDict>::iterator tIA, 
+                                Molecule   & tMol);
+    extern REAL checkProtonateN(std::vector<AtomDict>::iterator tIA, 
+                                Molecule   & tMol,
+                                REAL tPka,           REAL tPh);
+    extern REAL checkProtonateS(std::vector<AtomDict>::iterator tIA, 
+                                REAL tTolBondOrder);
+    extern REAL checkProtonateS(std::vector<AtomDict>::iterator tIA, 
+                                Molecule   & tMol);
+    extern REAL checkProtonateS(std::vector<AtomDict>::iterator tIA, 
+                                Molecule   & tMol,
+                                REAL tPka,           REAL tPh);
+    extern REAL checkProtonateC(std::vector<AtomDict>::iterator tIA, 
+                                REAL tTolBondOrder);
+    extern REAL checkProtonateC(std::vector<AtomDict>::iterator tIA, 
+                                Molecule   & tMol);
+    extern REAL checkProtonateC(std::vector<AtomDict>::iterator tIA, 
+                                Molecule   & tMol,
+                                REAL tPka,           REAL tPh);
+        
+    extern REAL  getTotalBondOrder(Molecule   & tMol,
+                                   std::vector<AtomDict>::iterator  tIA);
+    extern REAL  getBondOrder(Molecule   & tMol,
+                              int tIdx1, int tIdx2);
+    extern void setAllBondOrders(std::vector<AtomDict> & tAtoms,
+                                 std::vector<BondDict> & tBonds);
+    
+    extern void  setOneHAtomCoords();
+    
+    extern void  setOneHAtomCoordsSP3(std::vector<AtomDict> & tAtoms,
+                                      std::vector<AtomDict>::iterator tIA);
+    extern void  setOneHAtomCoordsSP2(std::vector<AtomDict> & tAtoms,
+                                      std::vector<AtomDict>::iterator tIA);
+    extern void  setOneHAtomCoordsSP(std::vector<AtomDict> & tAtoms,
+                                     std::vector<AtomDict>::iterator tIA);
+    
+    
+}
+
+#endif	/* CHEMPROPSET_H */
+
