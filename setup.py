852669b17e5d5124adc29eb4b76b6362343c0f8d
LucreziaCatapano <lucrezia@mrc-lmb.cam.ac.uk>
2025-02-20 17:34:56 +0000

    Update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="315",
     packages=find_packages(),
     install_requires=["rdkit", "servalcat",
-                      "aceDRG-tables @ git+https://github.com/flong-mrc/aceDRG-tables.git"],  # Add dependencies if needed
+                      "aceDRG-tables @ git+https://github.com/LucreziaCatapano/aceDRG-tables.git"],  # Add dependencies if needed
     entry_points={
         "console_scripts": [
             "acedrg=acedrg.acedrg:main",
