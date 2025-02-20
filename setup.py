f692cc838203085fa6cfd686fe40959424fbf715
LucreziaCatapano <lucrezia@mrc-lmb.cam.ac.uk>
2025-02-20 17:22:39 +0000

    Add requirements for acedrg-tables

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ setup(
     name="acedrg",
     version="315",
     packages=find_packages(),
-    install_requires=["rdkit", "servalcat"],  # Add dependencies if needed
+    install_requires=["rdkit", "servalcat",
+                      "aceDRG-tables @ git+https://github.com/flong-mrc/aceDRG-tables.git"],  # Add dependencies if needed
     entry_points={
         "console_scripts": [
             "acedrg=acedrg.acedrg:main",
